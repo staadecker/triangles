430d6ff71bb2f8ac4622692de29c5212168f0a46
Martin Staadecker <machstg@gmail.com>
2024-01-18 03:51:27 -0500

    Finish up!

--- triangles.js
+++ triangles.js
@@ -1,18 +1,33 @@
 let c = document.getElementById("canvas");
+
+// set canvas width to page width
+c.width = window.innerWidth * 0.9;
+c.height = window.innerHeight * 0.8;
+
 let ctx = c.getContext("2d");
 ctx.lineWidth = 0.5;
 const CIRCLE_SIZE = 6;
-const NUM_CIRCLES = 4;
+const NUM_CIRCLES = 10;
 const FORCE = 1;
 const DAMPING = 0.5;
-const TOWARDS_CENTER = 1;
-const SPRING = 0.01;
-const SPRING_LEN = 100;
+const TOWARDS_CENTER = 10;
+const TOWARDS_CENTER_DAMP = 0.6;
+const SPRING = 0.05;
+const SPRING_LEN = 200;
+const SPRING_DAMPEN = 0.4;
 const STOP_SPIN = 0.01;
+const MAX_SPEED = 300;
+const DEBUG = false;
+
+let avg_vx_acc = 0;
+let avg_vy_acc = 0;
 
 // Get canvas width
 const BOUNDS_X = c.width;
 const BOUNDS_Y = c.height;
+function forDrawY(y) {
+  return BOUNDS_Y - y;
+}
 const INNER_BOUNDS = 0.5;
 
 const items = [];
@@ -45,12 +60,43 @@ class Triangle {
     this.vx = 0;
     this.vy = 0;
     this.pos = pos;
+    this.spring1_acc = 0;
+    this.spring2_acc = 0;
+  }
+
+  dirToTarget() {
+    const link1 = items[this.link1];
+    const link2 = items[this.link2];
+    const dy = link2.y - link1.y;
+    const dx = link2.x - link1.x;
+    const angle = Math.atan2(dy, dx);
+    const r = Math.sqrt(dy ** 2 + dx ** 2);
+    const x1 = link1.x + r * Math.cos(angle + Math.PI / 3);
+    const y1 = link1.y + r * Math.sin(angle + Math.PI / 3);
+    const x2 = link1.x + r * Math.cos(angle - Math.PI / 3);
+    const y2 = link1.y + r * Math.sin(angle - Math.PI / 3);
+    const dist_1 = (x1 - this.x) ** 2 + (y1 - this.y) ** 2;
+    const dist_2 = (x2 - this.x) ** 2 + (y2 - this.y) ** 2;
+
+    const x = dist_1 < dist_2 ? x1 : x2;
+    const y = dist_1 < dist_2 ? y1 : y2;
+
+    if (DEBUG && this.pos == 0) {
+      ctx.beginPath();
+      ctx.arc(x, forDrawY(y), 2, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+
+    const dir_x = x - this.x;
+    const dir_y = y - this.y;
+
+    return [dir_x, dir_y];
   }
 
   draw() {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, CIRCLE_SIZE, 0, 2 * Math.PI);
-    if (this.pos == 0) {
+    ctx.arc(this.x, forDrawY(this.y), CIRCLE_SIZE, 0, 2 * Math.PI);
+    if (DEBUG && this.pos == 0) {
       ctx.fillStyle = "#00FF00";
     } else {
       ctx.fillStyle = "#000000";
@@ -59,13 +105,12 @@ class Triangle {
 
     ctx.strokeStyle = "#5F0000";
     ctx.beginPath();
-    // get mouse x position
 
     for (let link of [this.link1, this.link2]) {
-      ctx.moveTo(this.x, this.y);
+      ctx.moveTo(this.x, forDrawY(this.y));
       ctx.lineTo(
         (items[link].x - this.x) / 3 + this.x,
-        (items[link].y - this.y) / 3 + this.y
+        forDrawY((items[link].y - this.y) / 3 + this.y)
       );
     }
 
@@ -81,57 +126,37 @@ function setup() {
   for (let i = 0; i < NUM_CIRCLES; i++) {
     items.push(new Triangle(i));
   }
-  draw();
-}
-
-function draw() {
-  for (const item of items) {
-    item.draw();
-  }
-}
-
-function getTargets() {
-  let targets = [];
-  for (let i = 0; i < NUM_CIRCLES; i++) {
-    const node = items[i];
-    const link1 = items[node.link1];
-    const link2 = items[node.link2];
-    const dy = link2.y - link1.y;
-    const dx = link2.x - link1.x;
-    const angle = Math.atan2(dy, dx);
-    const r = Math.sqrt(dy ** 2 + dx ** 2);
-    const x1 = link1.x + r * Math.cos(angle + Math.PI / 3);
-    const y1 = link1.y + r * Math.sin(angle + Math.PI / 3);
-    const x2 = link1.x + r * Math.cos(angle - Math.PI / 3);
-    const y2 = link1.y + r * Math.sin(angle - Math.PI / 3);
-    const dist_1 = (x1 - node.x) ** 2 + (y1 - node.y) ** 2;
-    const dist_2 = (x2 - node.x) ** 2 + (y2 - node.y) ** 2;
-
-    const x = dist_1 < dist_2 ? x1 : x2;
-    const y = dist_1 < dist_2 ? y1 : y2;
-    targets.push([x, y]);
-  }
-  return targets;
 }
 
 function updatePosition(elapsed) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  const targets = getTargets();
   for (let i = 0; i < NUM_CIRCLES; i++) {
     const item = items[i];
-    const dir_x = targets[i][0] - item.x;
-    const dir_y = targets[i][1] - item.y;
-    item.vx = -item.vx * DAMPING + dir_x * FORCE;
-    item.vy = -item.vy * DAMPING + dir_y * FORCE;
+    const dirToTarget = item.dirToTarget();
+    item.vx += dirToTarget[0] * FORCE;
+    item.vy += dirToTarget[1] * FORCE;
 
-    for (const link in [item.link1, item.link2]) {
-      const linkItem = items[link];
-      const dx = item.x - linkItem.x;
-      const dy = item.y - linkItem.y;
-      const dist = Math.sqrt(dx ** 2 + dy ** 2);
-      item.vx -= dx * (dist - SPRING_LEN) * SPRING;
-      item.vy -= dy * (dist - SPRING_LEN) * SPRING;
+    const link1 = items[item.link1];
+    const dx = link1.x - item.x;
+    const dy = link1.y - item.y;
+    const dist = Math.sqrt(dx ** 2 + dy ** 2);
+    const error_in_dist = dist - SPRING_LEN;
+    item.spring1_acc *= SPRING_DAMPEN;
+    item.spring1_acc += error_in_dist * SPRING;
+    item.vx += dx * item.spring1_acc;
+    item.vy += dy * item.spring1_acc;
+
+    const link2 = items[item.link2];
+    const dx2 = link2.x - item.x;
+    const dy2 = link2.y - item.y;
+    const dist2 = Math.sqrt(dx2 ** 2 + dy2 ** 2);
+    const error_in_dist2 = dist2 - SPRING_LEN;
+    item.spring2_acc *= SPRING_DAMPEN;
+    item.spring2_acc += error_in_dist2 * SPRING;
+    item.vx += dx2 * item.spring2_acc;
+    item.vy += dy2 * item.spring2_acc;
+    
+    if (DEBUG && item.pos == 0) {
+        console.log(dist, item.spring1_acc);
     }
   }
 
@@ -140,15 +165,22 @@ function updatePosition(elapsed) {
   const avg_vx = items.reduce((acc, item) => acc + item.vx, 0) / NUM_CIRCLES;
   const avg_vy = items.reduce((acc, item) => acc + item.vy, 0) / NUM_CIRCLES;
 
-  ctx.beginPath();
-  ctx.arc(avg_x, avg_y, 2, 0, 2 * Math.PI);
-  ctx.fill();
-
-  const correct_x = (BOUNDS_X / 2 - avg_x) * TOWARDS_CENTER;
-  const correct_y = (BOUNDS_Y / 2 - avg_y) * TOWARDS_CENTER;
+  const offset_x = (avg_x - BOUNDS_X / 2) * TOWARDS_CENTER;
+  const offset_y = (avg_y - BOUNDS_Y / 2) * TOWARDS_CENTER;
+  avg_vx_acc *= TOWARDS_CENTER_DAMP;
+  avg_vy_acc *= TOWARDS_CENTER_DAMP;
+  avg_vx_acc += offset_x;
+  avg_vy_acc += offset_y;
   for (const item of items) {
-    item.vx += correct_x;
-    item.vy += correct_y;
+    item.vx -= avg_vx_acc;
+    item.vy -= avg_vy_acc;
+  }
+
+  if (DEBUG) {
+    ctx.beginPath();
+    ctx.arc(avg_x, forDrawY(avg_y), 2, 0, 2 * Math.PI);
+    ctx.arc(BOUNDS_X / 2, forDrawY(BOUNDS_Y / 2), 2, 0, 2 * Math.PI);
+    ctx.fill();
   }
 
   let moment = 0;
@@ -158,14 +190,14 @@ function updatePosition(elapsed) {
     const dy = item.y - avg_y;
     const dvx = item.vx - avg_vx;
     const dvy = item.vy - avg_vy;
-    
+
     const radius = Math.sqrt(dx ** 2 + dy ** 2);
     const speed = Math.sqrt(dvx ** 2 + dvy ** 2);
     const arm_angle = Math.atan2(dy, dx);
     const speed_angle = Math.atan2(dvy, dvx);
     const radial_velocity = speed * Math.sin(arm_angle - speed_angle);
     if (i == 0) {
-        // console.log(radius, speed, arm_angle * 180 / Math.PI, speed_angle * 180 / Math.PI, radial_velocity);
+      // console.log(radius, speed, arm_angle * 180 / Math.PI, speed_angle * 180 / Math.PI, radial_velocity);
     }
     item.vx -= Math.cos(arm_angle) * anti_spin * radius;
     item.vy -= Math.sin(arm_angle) * anti_spin * radius;
@@ -175,9 +207,13 @@ function updatePosition(elapsed) {
 
   anti_spin -= moment * STOP_SPIN;
 
-  console.log(moment, anti_spin);
+//   console.log(moment, anti_spin);
 
   for (const item of items) {
+    item.vx *= DAMPING;
+    item.vy *= DAMPING;
+    item.vx = Math.min(item.vx, MAX_SPEED);
+    item.vy = Math.min(item.vy, MAX_SPEED);
     item.x += item.vx * elapsed;
     item.y += item.vy * elapsed;
   }
@@ -188,8 +224,11 @@ let previousTime;
 function step(timeStamp) {
   const elapsed = timeStamp - previousTime;
   if (previousTime !== undefined) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     updatePosition(elapsed / 1000);
-    draw();
+    for (const item of items) {
+      item.draw();
+    }
   } else {
     previousTime = timeStamp;
     requestAnimationFrame(step);
